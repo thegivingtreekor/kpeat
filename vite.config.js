ab662bcc50390136c97d36a0306aebf546f9a2c8
seonyeonghun <sprax@naver.com>
2026-03-10 14:52:28 +0900

    설정 변경 1) 외부접속 설정 변경 - iptime ddns 접속용 2)  tailwindcss 적용을 위한 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss()
   ],
+  server: {
+    allowedHosts: [
+      'gtree.iptime.org'
+    ],
+    host: true
+  }
 })
