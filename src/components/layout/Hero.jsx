1f85ddb84d986eaf7568bdf9bb041e3cb82a495e
seonyeonghun <sprax@naver.com>
2026-03-10 20:38:18 +0900

    히어로 영역 1) daisyUI : 캐러셀 적용 2) 오토슬라이드

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 
 function Hero() {
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const totalSlides = 4
+  const autoPlayTime = 5000;
+
+  // 다음 슬라이드로 이동
+  const nextSlide = useCallback(() => {
+    setCurrentSlide((prev) => (prev === totalSlides ? 1: prev + 1))
+  }, [totalSlides])
+
+  // 이전 슬라이드 이동
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev === 1 ? totalSlides : prev - 1))
+  }
+
+  // 자동 타이머
+  useEffect(() => {
+    const element = document.getElementById(`slide-${currentSlide}`);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start'
+      })
+    }
+  }, [currentSlide])
   return (
-    <div>Hero</div>
+    <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide2" className="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" className="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
   )
 }
 
