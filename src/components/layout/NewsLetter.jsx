8b9bc8aca30e139148aeb258311d715a5a47f89c
seonyeonghun <sprax@naver.com>
2026-03-11 20:51:22 +0900

    뉴스레터 영역 1. 뉴스레터 가입용 폼

--- src/components/layout/NewsLetter.jsx
+++ src/components/layout/NewsLetter.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Latest() {
+function NewsLetter() {
   return (
-    <div>Latest</div>
+    <div className="container mx-auto">
+      <p className="text-sm">Subscribe to NewsLetter</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aut, non nesciunt at exercitationem dignissimos.</p>
+      <div className="newsletter">
+        <input
+          type="text"
+          placeholder="username@site.com"
+          className="input input-bordered join-item" />
+        <button className="btn btn-primary join-item">Subscribe</button>
+      </div>
+    </div>
   )
 }
 
-export default Latest
+export default NewsLetter
