eaf49525a2f5fc48d111fdff36dc1a7efe65f3b4
seonyeonghun <sprax@naver.com>
2026-03-12 10:34:52 +0900

    히어로 영역 1. 이미지 변경 : unsplash.com 2. 높이, 이미지 크기 변경

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -28,36 +28,36 @@ function Hero() {
   }, [currentSlide])
   return (
     <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
+  <div id="slide1" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-      className="w-full" />
+      src="https://images.unsplash.com/photo-1613856504577-3e8f30dbae38?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      className="w-full object-cover" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" className="btn btn-circle">❮</a>
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide2" className="carousel-item relative w-full">
+  <div id="slide2" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      className="w-full" />
+      src="https://images.unsplash.com/photo-1633854678155-7c0756840d27?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      className="w-full object-cover" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" className="btn btn-circle">❮</a>
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide3" className="carousel-item relative w-full">
+  <div id="slide3" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+      src="https://images.unsplash.com/photo-1760356786833-a4618d5efbf0?q=80&w=1072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" className="btn btn-circle">❮</a>
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide4" className="carousel-item relative w-full">
+  <div id="slide4" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+      src="https://plus.unsplash.com/premium_photo-1727114413040-0195795c1b42?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide3" className="btn btn-circle">❮</a>
