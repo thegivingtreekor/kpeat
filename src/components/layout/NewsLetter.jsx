1ac7b4842f15f88cdeaa1991b818e474104a7ffc
seonyeonghun <sprax@naver.com>
2026-03-12 09:54:18 +0900

    뉴스레터 영역 1. UI 변경 2. 폼 이벤트 적용

--- src/components/layout/NewsLetter.jsx
+++ src/components/layout/NewsLetter.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function NewsLetter() {
+  const [email, setEmail] = useState('')
+  const [disable, setDisable] = useState(false)
+  const subscribe = (email) => {
+    console.log(email);
+  }
   return (
-    <div className="container mx-auto">
-      <p className="text-sm">Subscribe to NewsLetter</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aut, non nesciunt at exercitationem dignissimos.</p>
+    <div className="container mx-auto py-5 text-center">
+      <p className="text-sm">뉴스레터 구독하기</p>
+      <p>피트모스 관련 최신 소식과 동향을 뉴스레터로 수신하려면 이메일을 등록하세요!</p>
       <div className="newsletter">
-        <input
-          type="text"
-          placeholder="username@site.com"
-          className="input input-bordered join-item" />
-        <button className="btn btn-primary join-item">Subscribe</button>
+        <input id="email"
+          type="email"
+          placeholder="news@kpeat.com"
+          className="input input-bordered join-item mr-1" value={email} onChange={(e) => setEmail(e.target.value)} onClick={() => setDisable()} />
+        <button className="btn btn-primary join-item bg-[#000000]">구독하기</button>        
       </div>
     </div>
   )
