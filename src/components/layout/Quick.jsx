8a6a4da4e217d2934fca7a07ebebf49800a659e9
seonyeonghun <sprax@naver.com>
2026-03-12 09:53:35 +0900

    빠른상품 영역 1. 데이터 구분을 위한 id 키 추가 2. 콘솔 오류 해결(key props)

--- src/components/layout/Quick.jsx
+++ src/components/layout/Quick.jsx
@@ -3,27 +3,27 @@ import React, { useState, useEffect } from 'react';
 function Quick() {
     const items = [
         {
-            title: 'ASB Gree',
+            id:1,title: 'ASB Gree',
             desc: 'a card component has a figure, a body part, and inside body there are title and actions parts',
             src: 'https://thumbnail.coupangcdn.com/thumbnails/remote/657x657q90trim/image/vendor_inventory/7de5/21f4ef1a006450ac595d1841d3782fbf331cc36eb556ed2b0f15eb6cbda4.jpg',
             alt: 'nordagri',
             category: 'peatmoss'
         },
         {
-            title: 'card title2',
+            id:2,title: 'card title2',
             desc: 'a card component has a figure, a body part, and inside body there are title and actions parts',
             src: 'https://thumbnail.coupangcdn.com/thumbnails/remote/657x657q90trim/image/vendor_inventory/5059/fc94721715f3e21734c543597e0a26af1d11ab05d8768ac9803e46041527.jpg',
             alt: 'sekyeong',
             category: 'peatmoss'
         },
         {
-            title: 'card title3',
+            id:3,title: 'card title3',
             desc: 'a card component has a figure, a body part, and inside body there are title and actions parts',
             src: 'https://thumbnail.coupangcdn.com/thumbnails/remote/657x657q90trim/image/vendor_inventory/7de5/21f4ef1a006450ac595d1841d3782fbf331cc36eb556ed2b0f15eb6cbda4.jpg',
             alt: 'greenpeat',
             category: 'peatmoss'
         },{
-            title: 'card title4',
+            id:4,title: 'card title4',
             desc: 'a card component has a figure, a body part, and inside body there are title and actions parts',
             src: 'https://thumbnail.coupangcdn.com/thumbnails/remote/657x657q90trim/image/vendor_inventory/5059/fc94721715f3e21734c543597e0a26af1d11ab05d8768ac9803e46041527.jpg',
             alt: 'sekyeong',
@@ -41,7 +41,7 @@ function Quick() {
             </ul>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
             {lists.map((list) => (
-                <div className="card bg-base-100 w-full shadow-sm">
+                <div className="card bg-base-100 w-full shadow-sm" key={list.id}>
                     <figure>
                         <img className="w-full h-auto md:aspect-auto object-cover"
                             src={list.src}
