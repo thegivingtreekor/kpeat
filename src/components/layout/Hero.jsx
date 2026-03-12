ff0e884717c1edd9d0ee4e33a22bb850178b8436
seonyeonghun <sprax@naver.com>
2026-03-12 15:51:12 +0900

    히어로 영역 1. static image 업로드 2. 임시 이미지 변경

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState, useCallback } from 'react'
-
+import img1 from '../../assets/peat_bog1.png'
+import img2 from '../../assets/peat_bog2.png'
+import img3 from '../../assets/peat_bog3.png'
+import img4 from '../../assets/peat_bog4.png'
 function Hero() {
   const [currentSlide, setCurrentSlide] = useState(1);
   const totalSlides = 4
@@ -30,7 +33,7 @@ function Hero() {
     <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://images.unsplash.com/photo-1613856504577-3e8f30dbae38?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src={img1}
       className="w-full object-cover" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" className="btn btn-circle">❮</a>
@@ -39,7 +42,7 @@ function Hero() {
   </div>
   <div id="slide2" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://images.unsplash.com/photo-1633854678155-7c0756840d27?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src={img2}
       className="w-full object-cover" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" className="btn btn-circle">❮</a>
@@ -48,7 +51,7 @@ function Hero() {
   </div>
   <div id="slide3" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://images.unsplash.com/photo-1760356786833-a4618d5efbf0?q=80&w=1072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src={img3}
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" className="btn btn-circle">❮</a>
@@ -57,7 +60,7 @@ function Hero() {
   </div>
   <div id="slide4" className="carousel-item relative w-full overflow-hidden h-100">
     <img
-      src="https://plus.unsplash.com/premium_photo-1727114413040-0195795c1b42?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src={img4}
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide3" className="btn btn-circle">❮</a>
